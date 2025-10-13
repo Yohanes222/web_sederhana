ee482da7ea51d4866489ae6564959cb92d243e79
Johanes Rafael <yohanesrafael80@gmail.com>
2025-10-13 23:18:55 +0700

    menambah beberapa controller

---
+++ app/Http/Controllers/authController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class authController extends Controller
+{
+    function redirect(){
+        return Socialite::driver('google')->redirect();
+    }
+
+    function callback(){
+            $user = Socialite::driver('google')->user();
+    $id = $user->id;
+    $email = $user->email;
+    $name = $user->name;
+
+    $user = User::updateOrCreate(
+        ['email' => $email],
+            ['name' => $name,
+                    'google_id' =>$id]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\authController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/redirect', [authController::class, "redirect"]);
+
+Route::get('/auth/callback', [authController::class, "callback"]); 
