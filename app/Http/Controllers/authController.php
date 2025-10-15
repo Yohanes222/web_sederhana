3dc71a80afea5895d8a50536986dd47a0c0bb070
Johanes Rafael <yohanesrafael80@gmail.com>
2025-10-15 17:14:54 +0700

    menambah halaman login

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -5,23 +5,46 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
 class authController extends Controller
 {
-    function redirect(){
+
+    function index(){
+        return view('auth.index');
+    }
+    function redirect()
+    {
         return Socialite::driver('google')->redirect();
     }
 
-    function callback(){
-            $user = Socialite::driver('google')->user();
-    $id = $user->id;
-    $email = $user->email;
-    $name = $user->name;
+    function callback()
+    {
+        $user = Socialite::driver('google')->user();
+        $id = $user->id;
+        $email = $user->email;
+        $name = $user->name;
 
-    $user = User::updateOrCreate(
-        ['email' => $email],
-            ['name' => $name,
-                    'google_id' =>$id]);
+        $cek = User::where('email', $email)->count();
+        if ($cek > 0) {
+            $user = User::updateOrCreate(
+                //array $attributes, kondisi pencarian (WHERE)
+                //array $values = []  data yang akan diupdate atau dibuat
+                ['email' => $email],
+                [
+                    'name' => $name,
+                    'google_id' => $id
+                ]
+            );
+            Auth::login($user);
+            return redirect()->to('dashboard');
+        }else {
+            return redirect()->to('auth')->with('error','akun yang anda masukkan tidak di izinkan untuk halaman admin');
+        }
+    }
+    public function logout(){
+        Auth::logout();
+        return redirect()->to('auth');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,25 @@
 <?php
 
-use App\Http\Controllers\authController;
+use Doctrine\DBAL\Logging\Middleware;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authController;
 use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/auth/redirect', [authController::class, "redirect"]);
+//Route::redirect('home','dashboard'); fungsi untuk memindahkan url dari home ke dashboard
 
-Route::get('/auth/callback', [authController::class, "callback"]); 
+Route::get('/auth', [authController::class, "index"])->name('login')->middleware('guest');
+
+Route::get('/auth/redirect', [authController::class, "redirect"])->middleware('guest');
+
+Route::get('/auth/callback', [authController::class, "callback"])->middleware('guest'); 
+
+Route::get('/auth/logout', [authController::class, "logout"]);
+
+Route::get('/dashboard',function(){
+    return 'Selamat datang '.Auth::user()->name.' di halaman admin';
+})->middleware('auth');
